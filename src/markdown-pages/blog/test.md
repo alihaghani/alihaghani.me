35d38f4f782ec0dde1fed003ce44c9e63ceb8922
Ali Haghani <alihaghani96@gmail.com>
2020-07-11 23:37:29 -0700

    Blog post template.

--- src/markdown-pages/blog/test.md
+++
@@ -1,7 +0,0 @@
----
-slug: "/blog/my-first-post"
-date: "2019-05-04"
-title: "My first blog post"
----
-
-Here's some quality content!
---
+++ src/templates/BlogPost.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default function BlogPost({ data }) {
+  const post = data.markdownRemark
+  return (
+    <Layout shouldRenderTitle shouldRenderFooter>
+      <SEO title={post.frontmatter.title}/>
+      <div className="page-content">
+        <h1>{post.frontmatter.title}</h1>
+        <h5>{`${post.frontmatter.date} · ${post.timeToRead} min read`}</h5>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      timeToRead
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+      }
+    }
+  }
+`
